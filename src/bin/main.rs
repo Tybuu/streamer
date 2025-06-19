fb1bc4844cc678f1aa68789867ffe88a7eaecb63
tybuu <tybuu1234@gmail.com>
2025-06-18 19:34:33 -0700

    init

---
+++ src/bin/main.rs
@@ -0,0 +1,32 @@
+use std::{io::Read, net::TcpListener};
+
+use enigo::{Enigo, Key, Keyboard, Settings};
+use window_server::codes::ScanCode;
+
+fn main() {
+    let addr = "192.168.10.3:8080";
+    let listener = TcpListener::bind(addr).expect("Failed to bind to address");
+    let mut dev = Enigo::new(&Settings::default()).unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => loop {
+                let mut buf = [0; size_of::<usize>()];
+                let buf_size = match stream.read_exact(&mut buf) {
+                    Ok(()) => usize::from_le_bytes(buf),
+                    Err(e) => panic!("{}", e),
+                };
+                let mut buffer = [0; 512];
+                match stream.read_exact(&mut buffer[..buf_size]) {
+                    Ok(()) => {
+                        println!("Data: {:?}", buffer);
+                        let code = bincode::deserialize::<ScanCode>(&buffer[..buf_size]).unwrap();
+                        println!("Code: {:?}", code);
+                        dev.key(code.to_enigo(), code.dir).unwrap();
+                    }
+                    Err(e) => panic!("{}", e),
+                }
+            },
+            Err(_) => todo!(),
+        }
+    }
+}
