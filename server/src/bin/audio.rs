363ce6c942de20ac342e817897ed987a270e8275
tybuu <tybuu1234@gmail.com>
2025-06-23 19:09:26 -0700

    changed server audio buffer sizes

--- server/src/bin/audio.rs
+++ server/src/bin/audio.rs
@@ -1,3 +1,5 @@
+use ringbuf::HeapRb;
+use ringbuf::traits::{Consumer, Observer, Producer, Split};
 use std::collections::VecDeque;
 use std::error;
 use std::io::Write;
@@ -15,74 +17,6 @@ type Res<T> = Result<T, Box<dyn error::Error>>;
 
 static START: AtomicBool = AtomicBool::new(false);
 
-// Playback loop, play samples received from channel
-fn playback_loop(rx_play: std::sync::mpsc::Receiver<Vec<u8>>) -> Res<()> {
-    let addr = "192.168.10.3:8080";
-    let listener = TcpListener::bind(addr).expect("Failed to bind to address");
-    loop {
-        for stream in listener.incoming() {
-            let mut stream = stream?;
-            START.store(true, std::sync::atomic::Ordering::Relaxed);
-            loop {
-                let mut data = rx_play.recv().unwrap();
-                stream.write_all(data.as_mut_slice())?;
-            }
-        }
-    }
-}
-
-// Capture loop, capture samples and send in chunks of "chunksize" frames to channel
-fn capture_loop(tx_capt: std::sync::mpsc::SyncSender<Vec<u8>>, chunksize: usize) -> Res<()> {
-    let device = get_default_device(&Direction::Capture)?;
-    let mut audio_client = device.get_iaudioclient()?;
-
-    let desired_format = WaveFormat::new(32, 32, &SampleType::Float, 44100, 2, None);
-
-    let blockalign = desired_format.get_blockalign();
-    debug!("Desired capture format: {:?}", desired_format);
-
-    let (def_time, min_time) = audio_client.get_device_period()?;
-    debug!("default period {}, min period {}", def_time, min_time);
-
-    let mode = StreamMode::EventsShared {
-        autoconvert: true,
-        buffer_duration_hns: min_time,
-    };
-    audio_client.initialize_client(&desired_format, &Direction::Capture, &mode)?;
-    debug!("initialized capture");
-
-    let h_event = audio_client.set_get_eventhandle()?;
-
-    let buffer_frame_count = audio_client.get_buffer_size()?;
-
-    let render_client = audio_client.get_audiocaptureclient()?;
-    let mut sample_queue: VecDeque<u8> = VecDeque::with_capacity(
-        100 * blockalign as usize * (1024 + 2 * buffer_frame_count as usize),
-    );
-    audio_client.start_stream()?;
-    loop {
-        while sample_queue.len() > (blockalign as usize * chunksize) {
-            debug!("pushing samples");
-            let mut chunk = vec![0u8; blockalign as usize * chunksize];
-            for element in chunk.iter_mut() {
-                *element = sample_queue.pop_front().unwrap();
-            }
-            if START.load(std::sync::atomic::Ordering::Relaxed) {
-                trace!("{:?}", chunk);
-                tx_capt.send(chunk)?;
-            }
-        }
-        trace!("capturing");
-        render_client.read_from_device_to_deque(&mut sample_queue)?;
-        if h_event.wait_for_event(1000000).is_err() {
-            error!("error, stopping capture");
-            audio_client.stop_stream()?;
-            break;
-        }
-    }
-    Ok(())
-}
-
 // Main loop
 fn main() -> Res<()> {
     let _ = SimpleLogger::init(
@@ -96,23 +30,28 @@ fn main() -> Res<()> {
 
     initialize_mta().ok()?;
 
-    let (tx_play, rx_play): (
-        std::sync::mpsc::SyncSender<Vec<u8>>,
-        std::sync::mpsc::Receiver<Vec<u8>>,
-    ) = mpsc::sync_channel(2);
-    let (tx_capt, rx_capt): (
-        std::sync::mpsc::SyncSender<Vec<u8>>,
-        std::sync::mpsc::Receiver<Vec<u8>>,
-    ) = mpsc::sync_channel(2);
-    let chunksize = 4096;
-
+    let (mut tx, mut rx) = HeapRb::new((128 * 4) * 4).split();
     // Playback
     let _handle = thread::Builder::new()
         .name("Player".to_string())
         .spawn(move || {
-            let result = playback_loop(rx_play);
-            if let Err(err) = result {
-                error!("Playback failed with error {}", err);
+            let addr = "192.168.10.3:8080";
+            let listener = TcpListener::bind(addr).expect("Failed to bind to address");
+            loop {
+                for stream in listener.incoming() {
+                    let mut buf = [0u8; 128 * 4];
+                    let mut stream = stream.unwrap();
+                    START.store(true, std::sync::atomic::Ordering::Relaxed);
+                    loop {
+                        let mut count = 0;
+                        while count != buf.len() {
+                            if !rx.is_empty() {
+                                count += rx.pop_slice(&mut buf[count..]);
+                            }
+                        }
+                        stream.write_all(buf.as_mut_slice()).unwrap();
+                    }
+                }
             }
         });
 
@@ -120,19 +59,48 @@ fn main() -> Res<()> {
     let _handle = thread::Builder::new()
         .name("Capture".to_string())
         .spawn(move || {
-            let result = capture_loop(tx_capt, chunksize);
-            if let Err(err) = result {
-                error!("Capture failed with error {}", err);
+            let device = get_default_device(&Direction::Capture).unwrap();
+            let mut audio_client = device.get_iaudioclient().unwrap();
+
+            let desired_format = WaveFormat::new(32, 32, &SampleType::Float, 44100, 2, None);
+
+            let blockalign = desired_format.get_blockalign();
+            debug!("Desired capture format: {:?}", desired_format);
+
+            let (def_time, min_time) = audio_client.get_device_period().unwrap();
+            debug!("default period {}, min period {}", def_time, min_time);
+
+            let mode = StreamMode::EventsShared {
+                autoconvert: true,
+                buffer_duration_hns: min_time,
+            };
+            audio_client
+                .initialize_client(&desired_format, &Direction::Capture, &mode)
+                .unwrap();
+            debug!("initialized capture");
+
+            let h_event = audio_client.set_get_eventhandle().unwrap();
+
+            let buffer_frame_count = audio_client.get_buffer_size().unwrap();
+
+            let render_client = audio_client.get_audiocaptureclient().unwrap();
+            // let mut sample_queue: VecDeque<u8> = VecDeque::with_capacity(
+            //     100 * blockalign as usize * (1024 + 2 * buffer_frame_count as usize),
+            // );
+            let mut buf = [0u8; 128 * 4];
+            audio_client.start_stream().unwrap();
+            loop {
+                let read = render_client.read_from_device(&mut buf).unwrap();
+                if START.load(std::sync::atomic::Ordering::Relaxed) {
+                    tx.push_slice(&buf[..read.0 as usize]);
+                }
+                if h_event.wait_for_event(1000000).is_err() {
+                    error!("error, stopping capture");
+                    audio_client.stop_stream().unwrap();
+                    break;
+                }
             }
         });
-
-    loop {
-        match rx_capt.recv() {
-            Ok(chunk) => {
-                debug!("sending");
-                tx_play.send(chunk).unwrap();
-            }
-            Err(err) => error!("Some error {}", err),
-        }
-    }
+    _handle.unwrap().join().unwrap();
+    Ok(())
 }
