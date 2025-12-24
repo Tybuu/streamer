e6eacef78e4799564e34df553f6a7a3578ab2649
tybuu <tybuu1234@gmail.com>
2025-12-24 14:32:05 -0800

    added wol

--- client/src/bin/main.rs
+++ client/src/bin/main.rs
@@ -2,6 +2,8 @@ use client::app::App;
 use client::display::DisplayControl;
 use client::stream::{Audio, Inputs, SharedSender};
 use shared::codes::HidEvent;
+use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
+use std::str::FromStr;
 use std::thread;
 use tokio::join;
 use tokio::net::TcpStream;
@@ -10,6 +12,8 @@ use tokio::sync::mpsc::{self, channel};
 use winit::event_loop::{ControlFlow, EventLoop};
 
 fn main() {
+    let mac_address: [u8; 6] = [0xD8, 0x5E, 0xD3, 0x85, 0x95, 0xEB];
+    wake_computer(mac_address).unwrap();
     let (hid_tx, hid_rx) = channel(128);
 
     thread::spawn(move || {
@@ -49,4 +53,20 @@ fn main() {
     event_loop.set_control_flow(ControlFlow::Wait);
     let mut app = App::new(hid_tx);
     event_loop.run_app(&mut app).unwrap();
+}
+
+fn wake_computer(mac_address: [u8; 6]) -> std::io::Result<()> {
+    let mut packet = Vec::with_capacity(102);
+    packet.extend_from_slice(&[0xFF; 6]);
+    for _ in 0..16 {
+        packet.extend_from_slice(&mac_address);
+    }
+
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    socket.set_broadcast(true)?;
+
+    let broadcast_addr = SocketAddrV4::new(Ipv4Addr::from_str("192.168.10.255").unwrap(), 9);
+
+    socket.send_to(&packet, broadcast_addr)?;
+    Ok(())
 }
